59202c98868c6ff2e34f2ef441e8ecbb3874b039
Koen Van Rulo <koen.vr@vecno.io>
2022-03-03 23:35:25 +0100

    reorg move market

--- arc-market/build.sh
+++ arc-market/build.sh
@@ -4,4 +4,4 @@ set -e
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 
 mkdir -p ../build
-cp ../target/wasm32-unknown-unknown/release/arc_assets.wasm ../build/arc-assets.wasm
+cp ../target/wasm32-unknown-unknown/release/arc_market.wasm ../build/arc-market.wasm
